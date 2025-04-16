b8324e4674f5257b9709105138bbbbf47beedd01
root <root@adhe-flash.flash.co.id>
2025-04-16 07:23:43 +0000

    update

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route(path: '/', name: 'app_frontend')]
+    function index(Request $request)
+    {
+        return $this->render('front_end/index.html.twig', []);
+    }
+}
