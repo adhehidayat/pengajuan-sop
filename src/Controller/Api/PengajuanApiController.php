821dcd21e4129d2ac89b6b499236c1d3b0333ddc
Adhe Hidhayat <iah@Adhes-MacBook-Pro.local>
2025-05-24 01:18:49 +0800

    update

--- src/Controller/Api/PengajuanApiController.php
+++ src/Controller/Api/PengajuanApiController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Api;
 
 use App\Components\Enum\PengajuanStatusEnum;
-use App\Entity\Narasumber;
 use App\Entity\Pengajuan;
 use App\Entity\PengajuanAttachment;
 use App\Entity\PengajuanProgress;
@@ -11,33 +10,17 @@ use App\Entity\RefLayanan;
 use App\Repository\NarasumberRepository;
 use App\Repository\PengajuanRepository;
 use App\Repository\RefLayananRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\Exception\ExceptionInterface;
-use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 
 class PengajuanApiController extends AbstractController
 {
-    public function __construct(private DenormalizerInterface $denormalizer)
-    {
-    }
 
-    /**
-     * @throws ExceptionInterface
-     */
     public function __invoke(Request $request, PengajuanRepository $pengajuanRepository, RefLayananRepository $refLayananRepository, NarasumberRepository $narasumberRepository): JsonResponse
     {
         $files = $request->files->get('attachments');
         $data = json_decode($request->request->get('data'), true);
-
-        $docs = new ArrayCollection();
-
-
 
         $parts = explode('/', $data['contract']);
 
@@ -48,7 +31,6 @@ class PengajuanApiController extends AbstractController
         $parts[3] = str_pad($repoPengajuan, 3, 0, STR_PAD_LEFT);
 
 
-        /** @var Pengajuan $pengajuan */
         $pengajuan = new Pengajuan();
         $pengajuan->setContract(implode('/', $parts));
         $pengajuan->setPengajuanProgress(
@@ -65,22 +47,11 @@ class PengajuanApiController extends AbstractController
                     ->setFile($file)
             );
         };
-        dump($pengajuan, $naraSumber);
+
         $pengajuanRepository->create($pengajuan);
 
 
-        return new JsonResponse([
-            'status' => 'ok',
-            'attachments' => $docs,
-            'data' => $data
-        ]);
+        return new JsonResponse($pengajuan, 201);
     }
 
-    /**
-     * @throws ExceptionInterface
-     */
-    public function denormalize($data, $type, $format = null, array $context = [])
-    {
-        return $this->denormalizer->denormalize($data, $type);
-    }
 }
