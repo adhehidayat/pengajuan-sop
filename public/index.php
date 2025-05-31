9c5aa0a6ba3f23e698dd4b3981637791fadf39f8
iah <adhe.hidayat89@gmail.com>
2025-06-01 00:40:08 +0800

    permission

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
+umask(0002); // This will let the permissions be 0775
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };
